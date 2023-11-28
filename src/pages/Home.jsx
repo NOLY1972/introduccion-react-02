f503c50a855c3fd87f11dc145b99dcfc43c6f528
Fernando <fernando30041972@hotmail.com>
2023-11-28 22:53:17 +0100

    Añadir componente home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+ 
+
+export default function Home() {
+    return (
+        <>
+         <Header />
+         <main>
+            <TituloPrincipal />
+            <Ejercicio1 />
+            {/*Añade tus componentes aqui */}
+         </main>
+         <Footer />
+         </>
+
+    )
+}
