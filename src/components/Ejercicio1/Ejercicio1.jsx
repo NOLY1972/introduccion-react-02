344713a91f6b963cacdcf9cf6590c48d580824f4
Fernando <fernando30041972@hotmail.com>
2023-11-28 22:52:10 +0100

    Añadir Ejercicio 1

--- src/components/Ejercicio1/Ejercicio1.jsx
+++ src/components/Ejercicio1/Ejercicio1.jsx
@@ -1,10 +1,13 @@
 import TituloEjercicio from '../TituloEjercicio'
+import PadreComponente from './PadreComponente'
 
 export default function Ejercicio1() {
   return (
     <section className="caja-ejercicio">
       <TituloEjercicio>Este es el ejercicio 1</TituloEjercicio>
       {/* Añade el resto del ejercicio aqui */}
+      <PadreComponente/>
     </section>
   )
 }
+
